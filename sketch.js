8049a41b615608e01c6ef299d73d51bae8712f83
gawa4423 <62973179+gawa4423@users.noreply.github.com>
2022-04-01 20:21:48 +0900

    first commit

---
+++ sketch.js
@@ -0,0 +1,253 @@
+
+//id属性で指定した要素を取得する
+let result = document.getElementById('p5page');
+
+
+//urlからパラメータを取得 http://127.0.0.1:5500/?p=dW5pdGU=
+let answer = atob(getParam('p'));;
+
+//解答を取得
+let textbox= document.getElementById("textbox");
+let userAnswer="";
+
+const speed=0.7;
+const dsize=0.7;
+let letterNum=5;
+
+let cGroup=[];
+
+
+let x,y;
+
+function setup() {
+    let canvasSize = Math.min(windowWidth,windowHeight*0.8)
+    let canvas= createCanvas(canvasSize,canvasSize);
+    canvas.parent(result);
+
+
+    console.log(answer);
+    setAnswer(answer);
+    
+    textSize(30*windowHeight/600)
+    textAlign(CENTER,CENTER);
+    colorMode(HSB, 100);
+    angleMode(DEGREES); 
+    background(50,0,0);
+    noStroke();
+    for(let i=0;i<letterNum;i++){
+        d = width*dsize/2; 
+        cGroup.push(new Dot(i,d,speed));
+    }
+    //console.log(cGroup)
+
+    //デバッグ用
+    //init();
+}
+
+  
+  
+  
+    //a=97,z=122
+  
+function draw() {
+
+    background(50,0,0,20);
+    //background(0,10);
+    
+    push();
+
+        translate(width/2,height*0.4)
+        //光の球
+        cGroup.forEach((item)=>{
+                item.drawMe();
+            }
+        );
+        //外側の円
+        noFill();
+        stroke(0,0,100,10);
+        circle(0,0,width*dsize);
+    
+    pop();
+
+        //下の単語欄
+        for(let i=0;i<letterNum;i++){
+            let size = width/letterNum*0.6;
+            let gap = (width-size*letterNum)/(letterNum+1);
+            let x = gap+size/2+(size+gap)*i;
+            let y = height*0.87
+
+            noFill();
+            stroke(90/letterNum*i,20,80);
+            rect(x-size/2,y-size/2,size,size);
+            noStroke();
+            fill(0,0,100);
+
+            if(userAnswer[i]){
+                text(userAnswer[i],x,y)
+            }else{
+                text("?",x,y)
+            }
+        }
+
+
+    
+}
+
+function init(){
+    cGroup=[];
+    for(let i=0;i<letterNum;i++){
+        let d= answer.charCodeAt(i)-userAnswer.charCodeAt(i);
+        let s=0;
+        if((d+26)%26 < (-d+26)%26){
+            //左回り
+            s=1
+        }else if((d+26)%26 > (-d+26)%26){
+            //右回り
+            s=-1
+        }else{
+            //反対側
+            s=0;
+        }
+        d = Math.min((d+26)%26,(-d+26)%26)/26;
+        let r = d*height*dsize;
+        s = s*(1+d)*speed;
+        cGroup.push(new Dot(i,r,s));
+        //console.log(i,answer[i]+"<->"+userAnswer[i],"d:"+d,"r:"+r,"s:"+s);  
+    }
+    //console.log(cGroup);
+}
+
+
+class Dot{
+    constructor(index,rNumber,sNumber,deg){
+        this.i=index;
+        this.c=color(90/letterNum*index,80,80);
+        this.x=0;
+        this.y=0;
+        this.angle=360*index/letterNum;
+        this.size=10;
+        this.r=rNumber;
+        this.speed=sNumber;
+        this.time=0;
+    }
+
+    drawMe(){
+        this.time++;
+        this.angle += this.speed;
+        this.x = this.r*Math.cos(this.angle/180*Math.PI)
+        this.y = this.r*Math.sin(this.angle/180*Math.PI)
+        
+        //blendMode(BLEND);
+        drawingContext.shadowBlur = this.size*3;
+        drawingContext.shadowColor = this.c;
+        //background(0,35);
+        blendMode(SCREEN);
+        fill(this.c);
+        circle(this.x,this.y,this.size);
+        // if(userAnswer[this.i]){
+        //     text(userAnswer[this.i],this.x,this.y)
+        // }else{
+        //     text("?",this.x,this.y)
+        // }
+        
+    }
+
+    statusUpdate(){
+
+    }
+}
+
+//answerをセット、もし不適ならデフォルトの表示に変更
+function setAnswer(ans){
+    ans = String(ans);
+    const regex = /^[a-z]+?$/g;
+    const found = ans?.match(regex);
+    if(3<=ans?.length && ans?.length<=7){
+        if(found && found[0]?.length===ans?.length){
+            console.log("true");
+            letterNum=ans.length;
+            return true;
+        }else{
+            alert("urlが正しくありません。デフォルトの問題を表示します。")
+            answer="unite";
+            letterNum=5;
+            return false;
+        }
+    }else{
+        alert("urlが正しくありません。デフォルトの問題を表示します。")
+        answer="unite";
+        letterNum=5;
+        return false;
+    }
+}
+
+
+
+function OnButtonClick(){
+    let tmpAns = textbox?.value;
+    let found = checkAlpha(tmpAns)
+
+    if(found && found.length===answer.length){
+        userAnswer = tmpAns;
+        //console.log(userAnswer);
+        init();
+    }else{
+        alert("小文字アルファベット"+letterNum+"文字で入力してください");
+    }
+}
+
+let mondaiText = document.getElementById('mondaiText');
+let mondaiInput = document.getElementById('mondaiInput');
+
+function createProblem(){
+    const url="https://gawa4423.github.io/circle/";
+    //"http://127.0.0.1:5500/"
+    //"https://gawa4423.github.io/circle/";
+
+    let mondaiAnswer = mondaiInput?.value;
+    let found = checkAlpha(mondaiAnswer);
+
+    //console.log(found);
+    if(!found){
+        alert("小文字アルファベット3〜7文字で入力してください");
+    }else if(found?.length<=2 || found?.length >= 8){
+        alert("小文字アルファベット3〜7文字で入力してください");
+    }else if(found?.length===mondaiAnswer?.length){
+        mondaiText.innerText = url + "?p=" + btoa(found);
+    }else{
+        alert("小文字アルファベット3〜7文字で入力してください");
+    }
+}
+
+//アルファベットからなる文字列か判定
+function checkAlpha(moji){
+    if(!moji){
+        return false;
+    }
+    let t = String(moji);
+    t = t.trim();
+    t =t.toLowerCase();
+    const regex = /^[a-z]+?$/g;
+    const found = t.match(regex);
+    if(found){
+        return found[0];
+    }else{
+        return false;
+    }
+}
+
+ /**
+ * Get the URL parameter value
+ *
+ * @param  name {string} パラメータのキー文字列
+ * @return  url {url} 対象のURL文字列（任意）
+ */
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
